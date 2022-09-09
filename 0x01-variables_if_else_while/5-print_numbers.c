11ebe670016de2ef6d7c48e18bd0a6cb966c0cd3
Godwin Obianagha <godwinobianagha@gmail.com>
2022-09-09 12:25:23 -0700

    print all single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always  (Success)
+*/
+
+int main(void)
+{
+	int a = '0';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
