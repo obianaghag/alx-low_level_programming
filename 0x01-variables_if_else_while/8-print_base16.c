8108283a4eaa844564f359813413b85e18cf7c6f
Godwin Obianagha <godwinobianagha@gmail.com>
2022-09-09 12:33:55 -0700

    print in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char a = '0';
+	char l = 'a';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (l = 'a'; l <= 'f'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
